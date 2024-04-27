aaa14f58f81d89248fd21405103471ce7efe93dc
chskTg <beingmsd77@gmail.com>
2024-04-27 14:32:05 -0400

    handling insert and delete

--- server.js
+++ server.js
@@ -3,20 +3,38 @@ const db = require("./database");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 
-app.post("/run-query", async (req, res) => {
+app.post("/query", async (req, res) => {
+  const { query, params } = req.body;
   try {
-    const { query } = req.body;
     if (!query) {
       return res.status(400).json({ error: "No query provided" });
     }
-    const result = await db.query(query);
+    const result = await db.query(query, params);
+    console.log("result", result);
     res.json(result.rows);
   } catch (error) {
     console.error("Error executing query:", error);
     res.status(500).json({ error: "Failed to execute query" });
+  }
+});
+
+app.post("/modify", async (req, res) => {
+  const { query, params } = req.body;
+  console.log("modify-query", query);
+  try {
+    if (!query) {
+      return res.status(400).json({ error: "No query provided" });
+    }
+    const result = await db.query(query, params);
+    console.log("modify-result", result);
+    res.json({ success: true, affectedRows: result.rowCount });
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Failed to modify database" });
   }
 });
 
